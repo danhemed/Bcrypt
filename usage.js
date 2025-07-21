68579ddccd6f5225e9a4e1499bca73a039dc1951
Dan <danhemed123@gmail.com>
2025-07-21 16:04:46 +0300

    create app.js

--- usage.js
+++
@@ -1,9 +0,0 @@
-import bcrypt from 'bcrypt';
-
-const password = 'mySecret123';
-
-const hashedPassword = await bcrypt.hash(password, 10);
-console.log(`hashed: ${hashedPassword}`);
-
-const isMatch = await bcrypt.compare(password, hashedPassword);
-console.log(`Match: ${isMatch}`);
