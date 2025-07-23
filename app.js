5f1cd51075d1866702d1063b85a6a5cc0b687c58
Dan <danhemed123@gmail.com>
2025-07-23 10:10:55 +0300

    finish the two of question 1 and 2

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import express from "express";
-import routerSingup from "./routes/router.signup.js"
+import routerUsers from "./routes/router.signup.js"
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/users", routerSingup);
+app.use("/users", routerUsers);
 
 app.listen(process.env.PORT || 3005, () => {
     console.log(`Server run...`);
